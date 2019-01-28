f5433a0864ef112fcbe98169114da1b33c35dc96
Gene <genep@gene-mac.local>
2019-01-27 20:35:01 -0800

    add error handling, unit tests

--- src/credentials.ts
+++ src/credentials.ts
@@ -1,10 +1,29 @@
 import AWS = require('aws-sdk');
 import os = require('os');
 import path = require('path');
+import fs = require('fs');
 
 const ini = require('prop-ini');
 
 export function setCredentials(profile: string, serialNumber: string, token: number) : Promise<void> {
+    const credPath = path.join(os.homedir(), '.aws');
+    const credFile = path.join(credPath, 'credentials');
+
+    if (!fs.existsSync(credFile)) {
+        return Promise.reject({
+            message: 'AWS credential file ~/.aws/credentials does not exist.'
+        });
+    }
+
+    const propIni = ini.createInstance({});
+    const awsCreds = propIni.decode({ file: credFile });
+
+    if (!awsCreds || !awsCreds.sections || Object.keys(awsCreds.sections).length <= 1) {
+        return Promise.reject({
+            message: 'AWS credential file does not contain any profiles.'
+        });
+    }
+
     return new Promise((resolve, reject) => {
         const sts = new AWS.STS();
 
@@ -13,17 +32,12 @@ export function setCredentials(profile: string, serialNumber: string, token: num
             TokenCode: `${token}`
         }, (err, data) => {
             if (err) {
+                console.error(err);
                 reject(err);
                 return;
             } else {
                 const newCreds = data.Credentials;
                 if (newCreds) {
-                    const credPath = path.join(os.homedir(), '.aws');
-                    const credFile = path.join(credPath, 'credentials');
-
-                    const propIni = ini.createInstance({});
-                    const awsCreds = propIni.decode({ file: credFile });
-
                     if (awsCreds.sections[profile]) {
                         propIni.addData(newCreds.AccessKeyId, profile, 'aws_access_key_id');
                         propIni.addData(newCreds.SecretAccessKey, profile, 'aws_secret_access_key');
---
+++ test/credentials.test.ts
@@ -0,0 +1,34 @@
+import credentials = require('../src/credentials');
+const fs = require('fs');
+const propIni = require('prop-ini');
+
+it(`should error with message when credentials file doesn't exist.`, async () => {
+    fs.existsSync = jest.fn();
+    fs.existsSync.mockReturnValue(false);
+
+    try {
+        await credentials.setCredentials('', '', 1);
+    } catch (e) {
+        expect(e).toEqual({
+            message: 'AWS credential file ~/.aws/credentials does not exist.'
+        });
+    }
+});
+
+it(`should error with mesage when credentials file is empty.`, async () => {
+    fs.existsSync = jest.fn();
+    fs.existsSync.mockReturnValue(true);
+
+    propIni.createInstance = jest.fn();
+    propIni.createInstance.mockReturnValue({
+        decode: () => {}
+    });
+
+    try {
+        await credentials.setCredentials('', '', 1);
+    } catch (e) {
+        expect(e).toEqual({
+            message: 'AWS credential file does not contain any profiles.'
+        });
+    }
+});
